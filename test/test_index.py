d9164419d7946ac396f909a16e34d375b8a73832
gg <gg@unit.no>
2019-10-15 11:24:19 +0200

    make code coverage work

---
+++ test/test_index.py
@@ -0,0 +1,42 @@
+import json
+import unittest
+from src import index
+
+
+class TestHandlerCase(unittest.TestCase):
+
+    def test_response(self):
+        uuid_regex = "[0-9a-fA-F]{8}\\-[0-9a-fA-F]{4}\\-[0-9a-fA-F]{4}\\-[0-9a-fA-F]{4}\\-[0-9a-fA-F]{12}"
+        print("testing response.")
+
+        event = {
+            'body': '{"url": "https://unit.no"}'
+        }
+
+        result = index.handler(event, None)
+        json_body = json.loads(result['body'])
+        print(result)
+        self.assertEqual(result['statusCode'], 200)
+        self.assertEqual(result['headers']['Content-Type'], 'application/json')
+        self.assertRegex(json_body['uuid'], uuid_regex, 'The returned value did not match the UUID pattern')
+        self.assertEqual(json_body['inputUrl'], 'https://unit.no')
+
+    def test_error_response(self):
+        uuid_regex = "[0-9a-fA-F]{8}\\-[0-9a-fA-F]{4}\\-[0-9a-fA-F]{4}\\-[0-9a-fA-F]{4}\\-[0-9a-fA-F]{12}"
+        print("testing response.")
+
+        event = {
+            'body': '{"uri": "https://unit.no"}'
+        }
+
+        result = index.handler(event, None)
+        json_body = json.loads(result['body'])
+        print(result)
+        self.assertEqual(result['statusCode'], 200)
+        self.assertEqual(result['headers']['Content-Type'], 'application/json')
+        self.assertNotEqual(json_body['inputUrl'], 'https://unit.no')
+        self.assertIsNone(json_body['inputUrl'])
+
+
+if __name__ == '__main__':
+    unittest.main()
